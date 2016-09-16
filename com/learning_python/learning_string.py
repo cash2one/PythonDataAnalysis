f9832ff5722371fa3b777c631c25eb131eb81fc3
Zealot <yizhou.tao@yinyuetai.com>
2016-09-16 20:22:08 +0800

    python project

--- com/learning_python/learning_string.py
+++ com/learning_python/learning_string.py
@@ -1,21 +1,26 @@
 __author__ = 'Zealot'
 
-a = 1
-b = 2
-x = a + b
-print x
 
-imperative = "abc"
-expletive = "123"
-s = '%s, %s!' % (imperative, expletive)
-print s
-x = '{}, {}!'.format(imperative, expletive)
-print x
-name = "eminem"
-n = 1
-s2 = 'name: %s; score: %d' % (name, n)
-print s2
-s3 = 'name: {}; score: {}'.format(name, n)
-print s3
-s4 = 'name: ' + name + '; score: ' + str(n)
-print s4
+def main():
+    a = 1
+    b = 2
+    x = a + b
+    print x
+
+    imperative = "abc"
+    expletive = "123"
+    s = '%s, %s!' % (imperative, expletive)
+    print s
+    x = '{}, {}!'.format(imperative, expletive)
+    print x
+    name = "eminem"
+    n = 1
+    s2 = 'name: %s; score: %d' % (name, n)
+    print s2
+    s3 = 'name: {}; score: {}'.format(name, n)
+    print s3
+    s4 = 'name: ' + name + '; score: ' + str(n)
+    print s4
+
+if __name__ == '__main__':
+    main()
