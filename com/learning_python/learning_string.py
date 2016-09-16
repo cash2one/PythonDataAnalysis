b794efc4f126efd0a7ec06491541f5411b918734
Zealot <yizhou.tao@yinyuetai.com>
2016-09-16 21:24:48 +0800

    python project

--- com/learning_python/learning_string.py
+++ com/learning_python/learning_string.py
@@ -24,5 +24,14 @@ def main():
     s4 = 'name: ' + name + '; score: ' + str(n)
     print s4
 
+    str1 = 'Hello World!'
+
+    print str1  # 输出完整字符串
+    print str1[0]  # 输出字符串中的第一个字符
+    print str1[2:5]  # 输出字符串中第三个至第五个之间的字符串
+    print str1[2:]  # 输出从第三个字符开始的字符串
+    print str1 * 2  # 输出字符串两次
+    print str1 + "TEST"  # 输出连接的字符串
+
 if __name__ == '__main__':
     main()
